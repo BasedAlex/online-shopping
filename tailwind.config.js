f115befd1e15801624cb832db9a2ee7583de65c4
Based <alexbroseph@mail.ru>
2022-09-28 18:37:56 +0400

    feat:button-fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,13 @@ module.exports = {
     },
     extend: {
       width: {
+        100: '25rem',
+        104: '26rem',
+        108: '27rem',
+        112: '28rem',
+        116: '29rem',
+        120: '30rem',
+        124: '31rem',
         128: '32rem',
       },
       gridColumn: {
